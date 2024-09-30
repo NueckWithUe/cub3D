08a5b66df0a76dc3a344a61a0a79f10248448d4d
Nils Nagel <nnagel@student.42heilbronn.de>
2024-09-30 15:47:14 +0200

    feat: added functions for extracting the data out of the configuration part of the scene

--- src/get_map.c
+++ src/get_map.c
@@ -6,11 +6,50 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 13:12:30 by nnagel            #+#    #+#             */
-/*   Updated: 2024/09/30 14:07:16 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/09/30 15:46:22 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/cub3D.h"
+
+static char	*get_path(char *line)
+{
+	char	*path;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	path = malloc(sizeof(char) * ft_strlen(line) + 1);
+	while (line[i] != ' ')
+		i++;
+	while (line[i] == ' ')
+		i++;
+	while (line[i])
+	{
+		if (line[i] == '\n')
+		{
+			path[j] = '\0';
+			break ;
+		}
+		path[j] = line[i];
+		i++;
+		j++;
+	}
+	return (path);
+}
+
+static void	get_conf(char *line)
+{
+	char	*data;
+
+	data = get_path(line);
+	// if (!ft_strncmp(line, "NO ", 3))
+	// {
+	// 	mlx_load_png(data);
+	// }
+	free(data);
+}
 
 static int	get_height(char **argv)
 {
@@ -47,7 +86,8 @@ char	**get_map(char **argv)
 	line = get_next_line(fd);
 	while (line[0] == '\n' || line[0] == 'N' || line[0] == 'S' || line[0] == 'E' || line[0] == 'W' || line[0] == 'F' || line[0] == 'C')
 	{
-
+		if (line[0] != '\n')
+			get_conf(line);
 		line = get_next_line(fd);
 	}
 	while (y <= height)
