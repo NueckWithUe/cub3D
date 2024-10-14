e79e0a84dd9470fdf7f146ff70db84d9e0c182d9
Nils Nagel <nnagel@student.42heilbronn.de>
2024-10-14 18:27:32 +0200

    feat: added a theoretical approach on how to complete the subject

--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 13:12:42 by nnagel            #+#    #+#             */
-/*   Updated: 2024/10/14 17:44:36 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/10/14 18:24:49 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 void	raycaster(void *param)
 {
-	t_data *d = (t_data *)param;
+	t_data	*d = (t_data *)param;
+	int		n;
+
 	d->ray->angle = d->player->angle;
-	int h = d->map->height;
-	int i = 0;
-	while (i < h)
-		i++;
+	n = 0;
+	while (n < FOV)
+	{
+		// claculate distance with horizontal lines
+		// calculate distance with vertical lines
+		// get the shortest distance
+		// convert casted ray into 3D line
+		n++;
+	}
 }
