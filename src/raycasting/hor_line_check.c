3c217600298ec0e93e39527fdef9a12af43a033d
Nils Nagel <nnagel@student.42heilbronn.de>
2024-10-16 19:12:43 +0200

    feat: horizontal and vertical lines now get calculated

--- src/raycasting/hor_line_check.c
+++ src/raycasting/hor_line_check.c
@@ -6,7 +6,7 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 10:27:09 by nnagel            #+#    #+#             */
-/*   Updated: 2024/10/15 10:46:22 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/10/16 18:59:19 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,24 @@ float	horizontal_line_check(t_data *data)
 	float	dist;
 
 	dist = 0;
-	(void)data;
+	if (data->ray->angle < M_PI)
+	{
+		data->ray->r_y = ((int)(data->player->pos_y / 64) * 64) - 0.0001;
+		data->ray->r_x = (data->ray->r_y - data->player->pos_y) * atanf(data->ray->angle) + data->player->pos_x;
+		data->ray->o_y -= 64;
+		data->ray->o_x = -data->ray->o_y * atanf(data->ray->angle);
+	}
+	else if (data->ray->angle > M_PI)
+	{
+		data->ray->r_y = ((int)(data->player->pos_y / 64) * 64) + 64;
+		data->ray->r_x = (data->ray->r_y - data->player->pos_y) * atanf(data->ray->angle) + data->player->pos_x;
+		data->ray->o_y = 64;
+		data->ray->o_x = -data->ray->o_y * atanf(data->ray->angle);
+	}
+	else if (data->ray->angle == 0 || data->ray->angle == M_PI)
+	{
+		data->ray->r_x = data->player->pos_x;
+		data->ray->r_y = data->player->pos_y;
+	}
 	return (dist);
 }
--- src/raycasting/ver_line_check.c
+++ src/raycasting/ver_line_check.c
@@ -6,7 +6,7 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 10:29:41 by nnagel            #+#    #+#             */
-/*   Updated: 2024/10/15 10:46:24 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/10/16 19:10:39 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,24 @@ float	vertical_line_check(t_data *data)
 	float	dist;
 
 	dist = 0;
-	(void)data;
+	if (data->ray->angle < 3 * (M_PI / 2) && data->ray->angle > (M_PI / 2))
+	{
+		data->ray->r_x = ((int)(data->player->pos_x / 64) * 64) - 0.0001;
+		data->ray->r_y = (data->ray->r_x - data->player->pos_x) * (tan(data->ray->angle) * -1) + data->player->pos_y;
+		data->ray->o_x -= 64;
+		data->ray->o_y = -data->ray->o_x * (tan(data->ray->angle) * -1);
+	}
+	else if (data->ray->angle < (M_PI / 2) || data->ray->angle > 3 * (M_PI / 2))
+	{
+		data->ray->r_x = ((int)(data->player->pos_x / 64) * 64) + 64;
+		data->ray->r_y = (data->ray->r_x - data->player->pos_x) * (tan(data->ray->angle) * -1) + data->player->pos_y;
+		data->ray->o_x = 64;
+		data->ray->o_y = -data->ray->o_x * (tan(data->ray->angle) * -1);
+	}
+	else if (data->ray->angle == 0 || data->ray->angle == M_PI)
+	{
+		data->ray->r_x = data->player->pos_y;
+		data->ray->r_y = data->player->pos_x;
+	}
 	return (dist);
 }
