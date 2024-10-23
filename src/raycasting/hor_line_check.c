82d09f56000f75212314b6d7ac33fbc4f5c6cb5e
Nils Nagel <nnagel@student.42heilbronn.de>
2024-10-23 17:06:04 +0200

    feat: improved the raycaster so it draws the map layout with the player and the rays, although needs fix for warping

--- src/raycasting/hor_line_check.c
+++ src/raycasting/hor_line_check.c
@@ -6,7 +6,7 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 10:27:09 by nnagel            #+#    #+#             */
-/*   Updated: 2024/10/16 18:59:19 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/10/23 16:31:18 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,48 @@
 float	horizontal_line_check(t_data *data)
 {
 	float	dist;
+	int		map_x;
+	int		map_y;
 
 	dist = 0;
 	if (data->ray->angle < M_PI)
 	{
 		data->ray->r_y = ((int)(data->player->pos_y / 64) * 64) - 0.0001;
-		data->ray->r_x = (data->ray->r_y - data->player->pos_y) * atanf(data->ray->angle) + data->player->pos_x;
-		data->ray->o_y -= 64;
-		data->ray->o_x = -data->ray->o_y * atanf(data->ray->angle);
+		data->ray->r_x = (data->ray->r_y - data->player->pos_y) / tanf(data->ray->angle) + data->player->pos_x;
+		data->ray->o_y = -64;
+		data->ray->o_x = -data->ray->o_y / tanf(data->ray->angle);
 	}
 	else if (data->ray->angle > M_PI)
 	{
 		data->ray->r_y = ((int)(data->player->pos_y / 64) * 64) + 64;
-		data->ray->r_x = (data->ray->r_y - data->player->pos_y) * atanf(data->ray->angle) + data->player->pos_x;
+		data->ray->r_x = (data->ray->r_y - data->player->pos_y) / tanf(data->ray->angle) + data->player->pos_x;
 		data->ray->o_y = 64;
-		data->ray->o_x = -data->ray->o_y * atanf(data->ray->angle);
+		data->ray->o_x = -data->ray->o_y / tanf(data->ray->angle);
 	}
-	else if (data->ray->angle == 0 || data->ray->angle == M_PI)
+	else
 	{
 		data->ray->r_x = data->player->pos_x;
 		data->ray->r_y = data->player->pos_y;
+		return (0);
+	}
+	while (1)
+	{
+		map_x = (int)(data->ray->r_x / 64);
+		map_y = (int)(data->ray->r_y / 64);
+		if (map_y >= 0 && map_y < data->map->height && map_x >= 0 && map_x < data->map->len)
+		{
+			if (data->map->con[map_y][map_x] == '1')
+			{
+				dist = sqrtf(powf((data->ray->r_x - data->player->pos_x), 2) +
+							powf((data->ray->r_y - data->player->pos_y), 2));
+				break;
+			}
+		}
+		else
+			break;
+		data->ray->r_x += data->ray->o_x;
+		data->ray->r_y += data->ray->o_y;
 	}
 	return (dist);
 }
+
--- src/raycasting/ver_line_check.c
+++ src/raycasting/ver_line_check.c
@@ -6,7 +6,7 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 10:29:41 by nnagel            #+#    #+#             */
-/*   Updated: 2024/10/16 19:10:39 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/10/23 16:36:38 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,48 @@
 float	vertical_line_check(t_data *data)
 {
 	float	dist;
+	int		map_x;
+	int		map_y;
 
 	dist = 0;
-	if (data->ray->angle < 3 * (M_PI / 2) && data->ray->angle > (M_PI / 2))
-	{
-		data->ray->r_x = ((int)(data->player->pos_x / 64) * 64) - 0.0001;
-		data->ray->r_y = (data->ray->r_x - data->player->pos_x) * (tan(data->ray->angle) * -1) + data->player->pos_y;
-		data->ray->o_x -= 64;
-		data->ray->o_y = -data->ray->o_x * (tan(data->ray->angle) * -1);
-	}
-	else if (data->ray->angle < (M_PI / 2) || data->ray->angle > 3 * (M_PI / 2))
+	if (data->ray->angle < M_PI_2 || data->ray->angle > 3 * M_PI_2)
 	{
 		data->ray->r_x = ((int)(data->player->pos_x / 64) * 64) + 64;
-		data->ray->r_y = (data->ray->r_x - data->player->pos_x) * (tan(data->ray->angle) * -1) + data->player->pos_y;
+		data->ray->r_y = (data->ray->r_x - data->player->pos_x) * tanf(data->ray->angle) + data->player->pos_y;
 		data->ray->o_x = 64;
-		data->ray->o_y = -data->ray->o_x * (tan(data->ray->angle) * -1);
+		data->ray->o_y = -data->ray->o_x * tanf(data->ray->angle);
 	}
-	else if (data->ray->angle == 0 || data->ray->angle == M_PI)
+	else if (data->ray->angle > M_PI_2 && data->ray->angle < 3 * M_PI_2)
 	{
-		data->ray->r_x = data->player->pos_y;
-		data->ray->r_y = data->player->pos_x;
+		data->ray->r_x = ((int)(data->player->pos_x / 64) * 64) - 0.0001;
+		data->ray->r_y = (data->ray->r_x - data->player->pos_x) * tanf(data->ray->angle) + data->player->pos_y;
+		data->ray->o_x = -64;
+		data->ray->o_y = -data->ray->o_x * tanf(data->ray->angle);
+	}
+	else
+	{
+		data->ray->r_x = data->player->pos_x;
+		data->ray->r_y = data->player->pos_y;
+		return (0);
+	}
+	while (1)
+	{
+		map_x = (int)(data->ray->r_x / 64);
+		map_y = (int)(data->ray->r_y / 64);
+		if (map_y >= 0 && map_y < data->map->height && map_x >= 0 && map_x < data->map->len)
+		{
+			if (data->map->con[map_y][map_x] == '1')
+			{
+				dist = sqrtf(powf((data->ray->r_x - data->player->pos_x), 2) +
+							powf((data->ray->r_y - data->player->pos_y), 2));
+				break;
+			}
+		}
+		else
+			break;
+		data->ray->r_x += data->ray->o_x;
+		data->ray->r_y += data->ray->o_y;
 	}
 	return (dist);
 }
+
