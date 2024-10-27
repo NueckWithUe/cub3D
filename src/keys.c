fbd58cbeaf79230d8a785e26ee19d257e6d1331a
Nils <nilsnagel04@gmail.com>
2024-10-27 20:23:10 +0100

    fix: removed debug statements for rotating view or moving

--- src/keys.c
+++ src/keys.c
@@ -17,7 +17,6 @@ static void	rotate(mlx_key_data_t keydata, t_player *p)
 	if ((keydata.key == MLX_KEY_Q
 			|| keydata.key == MLX_KEY_LEFT) && keydata.action == 2)
 	{
-		write(1, "View rotate left\n", 17);
 		p->angle -= 0.1;
 		if (p->angle < 0)
 			p->angle += 2 * M_PI;
@@ -27,7 +26,6 @@ static void	rotate(mlx_key_data_t keydata, t_player *p)
 	if ((keydata.key == MLX_KEY_E
 			|| keydata.key == MLX_KEY_RIGHT) && keydata.action == 2)
 	{
-		write(1, "View rotate right\n", 18);
 		p->angle += 0.1;
 		if (p->angle > 2 * M_PI)
 			p->angle -= 2 * M_PI;
@@ -40,25 +38,21 @@ static void	move(mlx_key_data_t keydata, t_player *p)
 {
 	if (keydata.key == MLX_KEY_W && keydata.action == 2)
 	{
-		write(1, "Move forward\n", 13);
 		p->pos_x += cos(p->angle) * 5;
 		p->pos_y += sin(p->angle) * 5;
 	}
 	if (keydata.key == MLX_KEY_A && keydata.action == 2)
 	{
-		write(1, "Move left\n", 10);
 		p->pos_x += cos(p->angle * M_PI) * 5;
 		p->pos_y += sin(p->angle * M_PI) * 5;
 	}
 	if (keydata.key == MLX_KEY_S && keydata.action == 2)
 	{
-		write(1, "Move backwards\n", 15);
 		p->pos_x -= cos(p->angle) * 5;
 		p->pos_y -= sin(p->angle) * 5;
 	}
 	if (keydata.key == MLX_KEY_D && keydata.action == 2)
 	{
-		write(1, "Move right\n", 11);
 		p->pos_x -= cos(p->angle) * 5;
 		p->pos_y -= sin(p->angle) * 5;
 	}
