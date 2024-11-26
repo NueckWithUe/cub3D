19c61f48f540e402e58cb615b9af35fd47c9c526
Nils Nagel <nnagel@student.42heilbronn.de>
2024-11-26 12:12:39 +0100

    fix: made the result look smoother

--- src/raycasting/convert_ray.c
+++ src/raycasting/convert_ray.c
@@ -6,93 +6,85 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 10:37:43 by nnagel            #+#    #+#             */
-/*   Updated: 2024/11/22 13:01:44 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/11/26 12:10:21 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/cub3D.h"
 
-// static uint32_t put_color(t_data *d)
-// {
-// 	uint32_t	north = 0xFF0000FF;
-// 	uint32_t	south = 0x00FF00FF;
-// 	uint32_t	east = 0x0000FFFF;
-// 	uint32_t	west = 0xFFFFFFFF;
-
-// 	if (d->ray->v_hit)
-// 	{
-// 		if (d->ray->angle > M_PI_2 && d->ray->angle < 3 * M_PI_2)
-// 			return (west);
-// 		else
-// 			return (east);
-// 	}
-// 	else
-// 	{
-// 		if (d->ray->angle < M_PI)
-// 			return (south);
-// 		else
-// 			return (north);
-// 	}
-// }
-
-static uint32_t put_color(t_data *d, int y, int height)
+static uint32_t put_color(t_data *d)
 {
-	mlx_texture_t	*tmp;
-	int				x_tex = 20;
-	int				y_tex;
-	uint32_t		*tex_color;
-	uint32_t		new_color;
+	uint32_t	north = 0xFF0000FF;
+	uint32_t	south = 0x00FF00FF;
+	uint32_t	east = 0x0000FFFF;
+	uint32_t	west = 0xFFFFFFFF;
 
-	y_tex = (((y * 256 - HEIGHT * 128 + height * 128) * TEX_HEIGHT) / height) / 256;
 	if (d->ray->v_hit)
 	{
 		if (d->ray->angle > M_PI_2 && d->ray->angle < 3 * M_PI_2)
-			tmp = d->twest;
+			return (west);
 		else
-			tmp = d->teast;
+			return (east);
 	}
 	else
 	{
 		if (d->ray->angle < M_PI)
-			tmp = d->tsouth;
+			return (south);
 		else
-			tmp = d->tnorth;
+			return (north);
 	}
-	tex_color = (uint32_t *)(tmp->pixels + (y_tex * TEX_WIDTH + x_tex) * sizeof(uint32_t));
-	new_color = ((*tex_color & 0xFF000000) >> 24) | ((*tex_color & 0x00FF0000) >> 8) | ((*tex_color & 0x0000FF00) << 8) | ((*tex_color & 0x000000FF) << 24);
-	return (new_color);
 }
+
+// static uint32_t put_color(t_data *d, int y, int height)
+// {
+// 	mlx_texture_t	*tmp;
+// 	int				x_tex = 20;
+// 	int				y_tex;
+// 	uint32_t		*tex_color;
+// 	uint32_t		new_color;
+
+// 	y_tex = (((y * 256 - HEIGHT * 128 + height * 128) * TEX_HEIGHT) / height) / 256;
+// 	if (d->ray->v_hit)
+// 	{
+// 		if (d->ray->angle > M_PI_2 && d->ray->angle < 3 * M_PI_2)
+// 			tmp = d->twest;
+// 		else
+// 			tmp = d->teast;
+// 	}
+// 	else
+// 	{
+// 		if (d->ray->angle < M_PI)
+// 			tmp = d->tsouth;
+// 		else
+// 			tmp = d->tnorth;
+// 	}
+// 	tex_color = (uint32_t *)(tmp->pixels + (y_tex * TEX_WIDTH + x_tex) * sizeof(uint32_t));
+// 	new_color = ((*tex_color & 0xFF000000) >> 24) | ((*tex_color & 0x00FF0000) >> 8) | ((*tex_color & 0x0000FF00) << 8) | ((*tex_color & 0x000000FF) << 24);
+// 	return (new_color);
+// }
 
 static void	draw_3d(t_data *d, float dist, int n)
 {
 	int	x;
-	int	end_x;
 	int	y;
 	int	end_y;
 	int	line_height;
 
-	x = n * (WIDTH / FOV);
-	end_x = x + (WIDTH / FOV);
-	if (n == FOV - 1)
-		end_x = WIDTH;
+	x = n;
 	line_height = ((CUB_SIZ * HEIGHT) / dist);
 	if (line_height > HEIGHT)
 		line_height = HEIGHT;
 	end_y = line_height + (HEIGHT / 2) - (line_height / 2);
-	while (x < end_x)
+	y = 0;
+	while (y < (HEIGHT / 2) - (line_height / 2))
+		mlx_put_pixel(d->ibuffer, x, y++, d->color_top);
+	while (y < end_y)
 	{
-		y = 0;
-		while (y < (HEIGHT / 2) - (line_height / 2))
-			mlx_put_pixel(d->ibuffer, x, y++, d->color_top);
-		while (y < end_y)
-		{
-			mlx_put_pixel(d->ibuffer, x, y, put_color(d, y, line_height));
-			y++;
-		}
-		while (y < HEIGHT)
-			mlx_put_pixel(d->ibuffer, x, y++, d->color_floor);
-		x++;
+		mlx_put_pixel(d->ibuffer, x, y, put_color(d));
+		y++;
 	}
+	while (y < HEIGHT)
+		mlx_put_pixel(d->ibuffer, x, y++, d->color_floor);
 }
 
 void	convert_ray(t_data *d, int hit_x, int hit_y, int n)
--- src/raycasting/raycasting.c
+++ src/raycasting/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: nnagel <nnagel@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 13:12:42 by nnagel            #+#    #+#             */
-/*   Updated: 2024/11/21 12:27:28 by nnagel           ###   ########.fr       */
+/*   Updated: 2024/11/26 12:09:48 by nnagel           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,23 +23,21 @@ static float	check_angle(float ray)
 
 void	raycaster(t_data *d)
 {
-	// t_data	*d;
 	int		n;
 	int		hit_x;
 	int		hit_y;
 
-	// d = (t_data *)param;
 	d->ray->angle = d->player->angle - (DR * (FOV / 2));
 	d->ray->angle = check_angle(d->ray->angle);
 	n = 0;
 	mlx_delete_image(d->mlx, d->ibuffer);
 	d->ibuffer = mlx_new_image(d->mlx, WIDTH, HEIGHT);
-	while (n < FOV)
+	while (n < WIDTH)
 	{
 		ddr(d, &hit_x, &hit_y);
 		convert_ray(d, hit_x, hit_y, n);
 		n++;
-		d->ray->angle += DR;
+		d->ray->angle += (DR * 60) / WIDTH;
 		d->ray->angle = check_angle(d->ray->angle);
 	}
 	mlx_image_to_window(d->mlx, d->ibuffer, 0, 0);
